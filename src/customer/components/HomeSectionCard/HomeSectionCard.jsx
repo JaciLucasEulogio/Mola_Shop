1ff8478bd2cbe532c40291c8843013aef1f72e1b
Jaci Lucas <72617519@continental.edu.pe>
2024-06-26 15:50:15 -0500

    Last Update

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const HomeSectionCard = ({product}) => {
-  
+const HomeSectionCard = ({ product }) => {
   return (
-    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border boder-black'>
-        <div className='h-[13rem] w-[10rem]'>
-            <img className='object-cover object-top w-full h-full' src={product.imageUrl} alt="" />
+    <div className='group relative overflow-hidden bg-white rounded-lg shadow-lg w-[15rem] mx-3 border border-gray-300 transition duration-300 transform hover:-translate-y-1 hover:shadow-xl'>
+      <div className='h-[13rem]'>
+        <img
+          className='object-cover w-full h-full'
+          src={product.imageUrl}
+          alt={product.title}
+        />
+      </div>
+      <div className='p-4'>
+        <h3 className='text-lg font-semibold text-gray-900 mb-2 group-hover:text-purple-600 transition duration-300'>{product.brand}</h3>
+        <p className='text-sm text-gray-700 mb-4'>{product.title}</p>
+        <div className='flex items-center justify-between'>
+          <p className='text-gray-900 font-medium text-lg'>$ {product.price}</p>
+          <button className='bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700 transition duration-300'>
+            Add to Cart
+          </button>
         </div>
-        <div className='p-4'>
-            <h3 className='text-lg font-medium text-gray-900'>{product.brand}</h3>
-            <p className='mt-2 text-sm text-gray-500'>{product.title}</p>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSectionCard
+export default HomeSectionCard;
